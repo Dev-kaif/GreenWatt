c96147a088e06ca7afc4329ae2f666fb4f55f0cf
Mohammad Kaif <kaifghalib123@gmail.com>
2025-06-21 02:49:17 +0530

    minor change

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -19,7 +19,6 @@ app.use(express.json());
 app.use(
   cors({
     origin: FRONTEND_URL,
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
