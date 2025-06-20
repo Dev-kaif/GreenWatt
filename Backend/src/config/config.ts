8caf6882f0ee231b47c8ca8542f3bf772838f4a8
Mohammad Kaif <kaifghalib123@gmail.com>
2025-06-21 02:45:51 +0530

    fixed cors

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const FRONTEND_URL = process.env.FRONTEND_URL as string
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY as string
 export const supabaseUrl = process.env.SUPABASE_URL as string;
 export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes';
-import meterReadingRoutes from './routes/meterReadingRoutes';
-import userProfileRoutes from './routes/userProfileRoutes';
-import applianceRoutes from './routes/applianceRoutes';
-import energyTipRoutes from './routes/energyTipRoutes'; 
-import analyticsRoutes from './routes/analyticsRoutes'; 
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+import meterReadingRoutes from "./routes/meterReadingRoutes";
+import userProfileRoutes from "./routes/userProfileRoutes";
+import applianceRoutes from "./routes/applianceRoutes";
+import energyTipRoutes from "./routes/energyTipRoutes";
+import analyticsRoutes from "./routes/analyticsRoutes";
+import cors from "cors";
+import { FRONTEND_URL } from "./config/config";
 
 dotenv.config();
 
@@ -15,13 +16,20 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/meter-readings', meterReadingRoutes);
-app.use('/api/profile', userProfileRoutes);
-app.use('/api/appliances', applianceRoutes);
-app.use('/api/energy-tips', energyTipRoutes); 
-app.use('/api/analytics', analyticsRoutes); 
+app.use("/api/auth", authRoutes);
+app.use("/api/meter-readings", meterReadingRoutes);
+app.use("/api/profile", userProfileRoutes);
+app.use("/api/appliances", applianceRoutes);
+app.use("/api/energy-tips", energyTipRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT);
